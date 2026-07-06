1ad281712724b293d2416a287ea7cdcf13aef9ec
mduram <mduc96@hotmail.com>
2026-07-06 16:35:32 +0200

    ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import "./globals.css";
+
 import Navbar from "@/components/Navbar";
 import SupportPrompt from "@/components/SupportPrompt";
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "The Rhino League",
   description: "Volleyball schedules, scores, standings, photos, and polls.",
+
   other: {
     "google-adsense-account": "ca-pub-7191954180081880",
   },
@@ -22,11 +27,22 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
+
         {children}
+
         <SupportPrompt />
+
         <SpeedInsights />
         <Analytics />
       </body>
+
+      <Script
+        id="google-adsense"
+        async
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7191954180081880"
+      />
     </html>
   );
 }
